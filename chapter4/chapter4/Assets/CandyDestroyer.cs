79de31bd3079ce8e2dd6ed2362b743cc8d07bac3
Kazuki MATSUDA / 松田一樹 <matsuda.kazuki@gmail.com>
2018-12-02 22:57:05 +0900

    Finish chapter4

---
+++ chapter4/chapter4/Assets/CandyDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDestroyer : MonoBehaviour {
+    public CandyHolder candyHolder;
+    public int reward;
+    public GameObject effectPrefab;
+    public Vector3 effectRotation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Candy")
+        {
+            Destroy(other.gameObject);
+             candyHolder.AddCandy(reward);
+
+            if (effectPrefab != null){
+                Instantiate(effectPrefab, other.transform.position, Quaternion.Euler(effectRotation));
+            }
+        }
+    }
+}
--- chapter4/chapter4/Assets/Shooter.cs
+++ chapter4/chapter4/Assets/Shooter.cs
@@ -2,34 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shooter : MonoBehaviour {
+public class Shooter : MonoBehaviour
+{
     const int SphereCandyFrequency = 3;
+    const int MaxShootPower = 5;
+    const int RecoverySeconds = 3;
 
     int sampleCandyCount;
+    int shotPower = MaxShootPower;
 
     public GameObject[] candyPrefabs;
     public GameObject[] candySquarePrefabs;
-    public GameObject candyHolder;
+    public CandyHolder candyHolder;
     public float shotSpeed;
     public float shotTorque;
     public float baseWidth;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonDown("Fire1")) Shot();
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-    GameObject SampleCandy(){
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) Shot();
+    }
+
+    GameObject SampleCandy()
+    {
         GameObject prefab = null;
-        if (sampleCandyCount % SphereCandyFrequency == 0){
+        if (sampleCandyCount % SphereCandyFrequency == 0)
+        {
             int index = Random.Range(0, candyPrefabs.Length);
             prefab = candyPrefabs[index];
-        } else {
+        }
+        else
+        {
             int index = Random.Range(0, candySquarePrefabs.Length);
             prefab = candySquarePrefabs[index];
         }
@@ -39,11 +49,51 @@ public class Shooter : MonoBehaviour {
         return prefab;
     }
 
-    public void Shot() {
-        GameObject candy = (GameObject)Instantiate(candyPrefab, transform.position, Quaternion.identity);
+    Vector3 GetInstantiatePosition()
+    {
+        float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
+        return transform.position + new Vector3(x, 0, 0);
+    }
+
+    public void Shot()
+    {
+        if (candyHolder.GetCandyAmount() <= 0)
+        {
+            return;
+        }
+
+        if (shotPower <= 0) return;
+
+        GameObject candy = (GameObject)Instantiate(SampleCandy(), GetInstantiatePosition(), Quaternion.identity);
+
+        candy.transform.parent = candyHolder.transform;
 
         Rigidbody candyRigidBody = candy.GetComponent<Rigidbody>();
         candyRigidBody.AddForce(transform.forward * shotSpeed);
         candyRigidBody.AddTorque(new Vector3(0, shotTorque, 0));
+
+        candyHolder.ConsumeCandy();
+
+        GetComponent<AudioSource>().Play();
+
+        ConsumePower();
+    }
+
+    void OnGUI(){
+        GUI.color = Color.black;
+
+        string label = "";
+        for (int i = 0; i < shotPower; i++) label = label + "+";
+        GUI.Label(new Rect(0, 15, 100, 30), label);
+    }
+
+    void ConsumePower() {
+        shotPower--;
+        StartCoroutine(RecoverPower());
+    }
+
+    IEnumerator RecoverPower(){
+        yield return new WaitForSeconds(RecoverySeconds);
+        shotPower++;
     }
 }
